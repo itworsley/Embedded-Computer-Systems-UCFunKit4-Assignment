e4ff17dc4b754ea5912224e62718d1eeed30b202
Josh Smith <jsm160@uclive.ac.nz>
2018-10-08 18:32:10 +1300

    Initial file comment header and skeleton for rsp function

--- game.c
+++ game.c
@@ -1,7 +1,19 @@
+/* ENCE260 2018 Group assignment
+ * Group 211
+ * Joshua Smith - jsm160
+ * Isaac Worsley - 
+ * */
+
 #include "system.h"
+
+int rps(char pChoice, char oChoice)
+{
+	
+}
 
 int main (void)
 {
+	
     system_init ();
 
 
