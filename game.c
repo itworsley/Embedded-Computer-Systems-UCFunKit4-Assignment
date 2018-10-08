a36c0d5d104f8cbb1218438f001724b76255ffc1
Joshua William Gillespie Smith <jsm160@cs18227kq.canterbury.ac.nz>
2018-10-09 11:50:53 +1300

    Added options string for options of RPS and allowed browsing of the 3 characters using the navswitch and limited it to just those characters

--- game.c
+++ game.c
@@ -1,10 +1,10 @@
 /* ENCE260 2018 Group assignment
  * Group 211
  * Joshua Smith - jsm160
- * Isaac Worsley - 
+ * Isaac Worsley -
  * */
 
-/* Module declarations */ 
+/* Module declarations */
 #include "system.h"
 #include "ledmat.h"
 #include "navswitch.h"
@@ -14,32 +14,56 @@
 
 /* Define polling rate in Hz.  */
 #define LOOP_RATE 300
+#define MESSAGE_RATE 10
 
 //int rps(char pChoice, char oChoice)
 //{
-	//return 0;
+    //return 0;
 //}
+
+void display_character (char character)
+{
+    char buffer[2];
+    buffer[0] = character;
+    buffer[1] = '\0';
+    tinygl_text (buffer);
+}
 
 int main (void)
 {
-	/* System init */
+    /* System init */
     system_init();
-	ledmat_init();
-	navswitch_init();
-	tinygl_init(LOOP_RATE);
-	pacer_init(LOOP_RATE);
-	
-	tinygl_font_set(&font5x7_1);
-	tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
-	tinygl_text_dir_set (TINYGL_TEXT_DIR_NORMAL);
+    ledmat_init();
+    navswitch_init();
+    tinygl_init(LOOP_RATE);
+    pacer_init(LOOP_RATE);
 
-	tinygl_text("RPS");
-	
+    tinygl_font_set(&font5x7_1);
+    tinygl_text_mode_set(TINYGL_TEXT_MODE_STEP);
+    tinygl_text_dir_set (TINYGL_TEXT_DIR_NORMAL);
+    tinygl_text_speed_set(MESSAGE_RATE);
+
+    char options[4];
+    int i = 0;
+    options[0] = 'R';
+    options[1] = 'P';
+    options[2] = 'S';
+    options[3] = '\0';
+    char character = options[i];
+
+
     while (1)
     {
-	navswitch_update();
-	tinygl_update();
-	pacer_wait();
+        pacer_wait ();
+        tinygl_update ();
+        navswitch_update ();
+
+        if (navswitch_push_event_p (NAVSWITCH_NORTH) && i < 2)
+            character = options[++i];
+
+        if (navswitch_push_event_p (NAVSWITCH_SOUTH) && i > 0)
+            character = options[--i];
+        display_character(character);
     }
     return 0;
 }
